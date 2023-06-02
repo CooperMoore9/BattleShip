7e077e6fe73d00af49058209fdf42e860d428cb9
CooperMoore9 <thefryman9@gmail.com>
2023-06-02 15:29:41 -0500

    html stuff

--- dist/gameBoardLogic.js
+++ dist/gameBoardLogic.js
@@ -1,2 +1,8 @@
 "use strict";
+// Factory Function for gameBoard
+// needs to be a factory function because im gonna need to make 2 per game
+// one for the player to place pieces and one for the player to click on to shoot at
+// First things first, generate a grid?
+// be able to track every part of the board
+//
 function gameBoard() { }
